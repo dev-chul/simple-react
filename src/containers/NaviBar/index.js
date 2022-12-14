74ee9d335357e5a1872181b74be5d52183b330d3
lostjcwide <lostjcwide@naver.com>
2022-12-14 14:32:16 +0900

    .

---
+++ src/containers/NaviBar/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+
+import BottomNavigation from 'reactjs-bottom-navigation'
+import 'reactjs-bottom-navigation/dist/index.css'
+
+import { HomeOutlined, SearchOutlined, BellOutlined, MenuOutlined } from '@ant-design/icons';
+
+export default function NaviBar() {
+  const bottomNavItems = [
+    {
+      title: 'Home',
+      icon: <HomeOutlined style={{ fontSize: '18px' }} />,
+      activeIcon: <HomeOutlined style={{ fontSize: '18px', color: '#fff' }} />
+    },
+    {
+      title: 'Search',
+      icon: <SearchOutlined style={{ fontSize: '18px' }} />,
+      activeIcon: <SearchOutlined style={{ fontSize: '18px', color: '#fff' }} />
+    },
+    {
+      title: 'Notifications',
+      icon: <BellOutlined style={{ fontSize: '18px' }} />,
+      activeIcon: <BellOutlined style={{ fontSize: '18px', color: '#fff' }} />
+    },
+    {
+      title: 'Menu',
+      icon: <MenuOutlined style={{ fontSize: '18px' }} />,
+      activeIcon: <MenuOutlined style={{ fontSize: '18px', color: '#fff' }} />,
+      onClick: () => alert('menu clicked')
+    }
+  ]
+  
+  return (
+    <div>
+      <BottomNavigation
+        items={bottomNavItems}
+        defaultSelected={0}
+        onItemClick={(item) => console.log(item)}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import NaviBar from './containers/NaviBar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <NaviBar />
   </React.StrictMode>
 );
 

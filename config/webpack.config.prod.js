a23404682c0f2c8329d6a3536b46ac9d0c505b7f
dev-chul <lostjcwide@gmail.com>
2022-12-16 18:04:18 +0900

    .

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   
   output: {
     path: path.join(__dirname, "../dist"),

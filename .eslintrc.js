4baf01c82bb391a80963aa7506bb6a212eeb5fc1
dev-chul <lostjcwide@gmail.com>
2022-12-17 18:41:46 +0900

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,12 @@ module.exports = {
     rules: {
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'react/react-in-jsx-scope': [0],
+        'react/self-closing-comp': [0],
+        'react/jsx-curly-brace-presence': [0],
         'import/extensions': [0, { ignorePackages: true }],
         'import/prefer-default-export': [0],
         'no-undef': [0],
-        'react/self-closing-comp': [0],
-        'react/jsx-curly-brace-presence': [0],
         'import/no-unresolved': [0],
+        'jsx-a11y/anchor-is-valid': [0],
     },
 };

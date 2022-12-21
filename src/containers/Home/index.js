6a6930d618fb0e4ef78f53b52c06c3673404cc81
dev-chul <lostjcwide@gmail.com>
2022-12-21 13:39:08 +0900

    .

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -80,7 +80,9 @@ export default function Home() {
         <div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>This app is developed using React. By. {name}</p>
+                <p>
+                    This app is made using React. <br /> By. {name}
+                </p>
                 <a className="App-link" href="#" rel="noopener noreferrer">
                     Learn React
                 </a>

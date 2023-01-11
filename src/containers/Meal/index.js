68e58291ca00f9d4be991dcff9d72691647d7054
dev-chul <lostjcwide@gmail.com>
2023-01-11 12:46:44 +0900

    .

--- src/containers/Meal/index.js
+++ src/containers/Meal/index.js
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Meal() {
+    const [geoX, setGeoX] = useState(0);
+    const [geoY, setGeoY] = useState(0);
+
     useEffect(() => {
         if ('geolocation' in navigator) {
             /* 위치정보 사용 가능 */
@@ -14,12 +17,20 @@ export default function Meal() {
 
     const onSuccess = geoData => {
         console.log(geoData.coords.longitude);
+        setGeoX(geoData.coords.longitude);
         console.log(geoData.coords.latitude);
+        setGeoY(geoData.coords.latitude);
     };
 
     const onError = err => {
         console.log(err);
     };
 
-    return <div>안녕하세요.</div>;
+    return (
+        <div>
+            안녕하세요. <br />
+            x: {geoX} <br />
+            y: {geoY}
+        </div>
+    );
 }

81fbc6fc76d52c5c8f70c602a34e12e8956a8617
dev-chul <lostjcwide@gmail.com>
2023-01-12 08:44:24 +0900

    .

--- env/dev.env.js
+++ env/dev.env.js
@@ -1,7 +1,7 @@
 module.exports = {
     NODE_ENV: '"development"',
     BASE_API: '"https://146.56.174.154:9453"',
-    BASE_OAUTH: '"http://146.56.174.154:9080"',
+    BASE_OAUTH: '"https://146.56.174.154:9443"',
     BASE_CDN: '"http://146.56.174.154:8100"',
     RUN_ANALYZER: false,
 };

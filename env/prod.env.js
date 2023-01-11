b49d914a4c076fdb15a2818e7e8aeebe00ad39ba
dev-chul <lostjcwide@gmail.com>
2023-01-12 08:33:38 +0900

    .

--- env/prod.env.js
+++ env/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"http://146.56.174.154:9090"',
+    BASE_API: '"https://146.56.174.154:9453"',
     BASE_OAUTH: '"http://146.56.174.154:9080"',
     BASE_CDN: '"http://146.56.174.154:8100"',
     RUN_ANALYZER: true,
